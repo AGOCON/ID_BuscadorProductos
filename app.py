60f251bbb65ee48c7faba6ed132c9931a4be0789
vimsanchez <vimsanchez@agoconsultores.com.mx>
2022-10-12 12:25:18 -0500

    v0.1

---
+++ app.py
@@ -0,0 +1,56 @@
+import pyautogui
+from funciones import consultaTienda
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="random",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get help': 'mailto:mesaayudaago@agoconsultores.com.mx',
+        'Report a bug': None,
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
+@st.cache
+def exportar_csv(df):
+    return df.to_csv(index=None)
+
+st.title('Consulta de productos auditados')
+
+tienda = st.number_input('Escribe el no. de tienda', key='input_tienda', step=1)
+
+if tienda == 0:
+    pass
+
+else:
+    try:
+        resumen, detalle = consultaTienda(tienda)
+        if len(detalle) == 0:
+            st.write('Esa tienda no existe, por favor consulta otra')
+        else:
+            st.write('A continuación se muestran los productos auditados en la última visita a la tienda:')
+            st.subheader(f'{detalle.Tienda.head(1).values[0]} ({detalle.Formato.head(1).values[0]})')
+            tab1, tab2 = st.tabs(['Resumen','Detalle'])
+            with tab1:
+                st.write(detalle[['Inicio Monitoreo','Fin Monitoreo','Cierre Tienda']])
+                st.write(pd.DataFrame(data = {'UPCs auditados': [len(resumen)], 'Frentes leídos': [resumen.Frentes.sum()]}))
+            with tab2:
+                st.download_button(
+                    'Exportar a CSV',
+                    exportar_csv(resumen[['Categoria','SubCategoria','Segmento','SubSegmento','UPC','Descripcion','Frentes']]),
+                    'Productos.csv',
+                    'text/csv'
+                )
+                st.dataframe(resumen[['Categoria','SubCategoria','Segmento','SubSegmento','UPC','Descripcion','Frentes']])
+
+    except Exception as e:
+        st.write('Ha habido un error, por favor comunique el siguiente código a mesaayudaago@agoconsultores.com.mx', e)
+        pass
+
+    btn_reiniciar = st.button('Reiniciar')
+    if btn_reiniciar:
+        pyautogui.hotkey("ctrl","F5")
---
+++ funciones.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+import pyodbc
+
+def consultaTienda(tienda):
+    sql_con = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};SERVER=192.168.1.71,1433;DATABASE=AgoSorianaHist;UID=usrBIVentas;PWD=@goXc8rW;')
+    tsql = f'''
+    SELECT
+        E.Clasificacion Categoria,
+        D.Clasificacion SubCategoria,
+        C.Clasificacion Segmento,
+        B.Clasificacion SubSegmento,
+        A.UPC, A.Descripcion, A.Frentes
+    FROM (
+        SELECT
+            A.*,
+            B.PROD_NAME Descripcion, B.CATEGORY
+        FROM (
+            SELECT A.UPCERR UPC, COUNT(A.UPCERR) Frentes
+            FROM tbl_PositionRealogramaMonitoreo A
+                JOIN (
+                    SELECT A.IdRealograma
+                    FROM tbl_RealogramaMonitoreo A
+                        JOIN (
+                            SELECT TOP 1 Clave_Tienda IdTienda, Evento
+                            FROM tbl_BitacoraTiendasProcesadas
+                            WHERE Clave_Tienda = {tienda} AND Tienda_Cerrada = 1
+                            ORDER BY Evento DESC
+                        ) B ON A.IdTienda = B.IdTienda AND A.Evento = B.Evento
+                ) B ON A.IdRealograma = B.IdRealograma
+            GROUP BY A.UPCERR
+        ) A
+            LEFT JOIN cat_Productos B ON A.UPC = B.UPC
+    ) A
+        LEFT JOIN cat_Categorias B ON A.CATEGORY = B.ClaveClasificacion
+        LEFT JOIN cat_Categorias C ON B.OrgNode.GetAncestor(1) = C.OrgNode
+        LEFT JOIN cat_Categorias D ON B.OrgNode.GetAncestor(2) = D.OrgNode
+        LEFT JOIN cat_Categorias E ON B.OrgNode.GetAncestor(3) = E.OrgNode
+    WHERE B.Clasificacion != 'Raiz'
+    ORDER BY Categoria, SubCategoria, Segmento, SubSegmento, Descripcion
+    '''
+    dfProductos = pd.read_sql(tsql, sql_con)
+
+    tsql = f'''
+    SELECT
+        B.Tienda,
+        C.Formato,
+        A.Clave_Tienda IdTienda, Evento,
+        CAST(A.Fecha_Inicio_Monitoreo AS DATE) [Inicio Monitoreo],
+        CAST(A.Fecha_Fin_Monitoreo AS DATE) [Fin Monitoreo],
+        CAST(A.Fecha_Tienda_Cerrada AS DATE) [Cierre Tienda]
+    FROM (
+        SELECT TOP 1 *
+        FROM tbl_BitacoraTiendasProcesadas
+        WHERE Clave_Tienda = {tienda} AND Tienda_Cerrada = 1
+        ORDER BY Evento DESC
+    ) A
+        JOIN cat_Tiendas B ON A.Clave_Tienda = B.IdTienda
+        JOIN cat_Formatos C ON B.IdFormato = C.IdFormato
+    '''
+    dfTienda = pd.read_sql(tsql, sql_con)
+
+    return dfProductos, dfTienda
